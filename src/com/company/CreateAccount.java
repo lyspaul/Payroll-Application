eefcdfd5ccb1d94b57f666cc662461bf2edc33c7
Lys-Paul Lukuka <lyslukuka@gmail.com>
2021-02-13 01:07:17 -0600

    I started working on the create account page.

---
+++ src/com/company/CreateAccount.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CreateAccount extends JFrame {
+    String employeeID;
+    private JTextField employeeIDField = new JTextField();
+    String firstName;
+    private JTextField firstNameField = new JTextField();
+    String lastName;
+    private JTextField lastNameField = new JTextField();
+    String month;
+    String day;
+    String year;
+    String username;
+    private JTextField usernameField = new JTextField();
+    String password;
+    private JPasswordField passwordField = new JPasswordField();
+    String emailAddress;
+    private JTextField emailAddressField = new JTextField();
+    String address;
+    private TextField addressField = new TextField();
+    String city;
+    private TextField cityField = new TextField();
+    String zipcode;
+    private TextField zipcodeField = new TextField();
+
+}
